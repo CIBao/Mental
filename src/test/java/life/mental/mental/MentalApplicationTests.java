95ace3518f3ad2bcc7becfd84153f032e5061bf7
CIBao <2861596304@qq.com>
2020-01-02 22:40:51 +0800

    init repo

---
+++ src/test/java/life/mental/mental/MentalApplicationTests.java
@@ -0,0 +1,13 @@
+package life.mental.mental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
